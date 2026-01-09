28e91a896ec57c6ff518ecf6e7465a35f0a38c69
Samruddhi Adhikary <136476753+Samruddhi345A@users.noreply.github.com>
2026-01-09 23:04:26 +0530

    Completed the text Analyzer

--- TextAnalyzer/src/App.jsx
+++ TextAnalyzer/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './App.css'
 function App() {
+  const [text, setText] = useState("");
+  let textCount = text.length
+  let wordCount = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+
+  const handleChange = (e) => {
+    setText(() => (e.target.value))
+  }
+
   return (
-    <div>App</div>
+    <>
+      <div className='min-h-screen flex flex-col justify-center items-center  '>
+        <input id='textField' className="items-center text-center justify-center border-b border-b-blue-950 focus:outline-none" value={text} onChange={handleChange} placeholder='Enter Text' />
+        <div className='p-1 m-7 flex flex-row'>
+          <p className='p-3 text-center font-thin text-sm'>
+            Character count: {textCount}
+          </p>
+          <p className='p-3 text-center font-thin text-sm'>
+            Word count: {wordCount}
+          </p>
+        </div>
+        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2' onClick={() => setText(text.toUpperCase())}>Convert to Uppercase</button>
+        <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded m-2' onClick={() => setText(text.toLowerCase())}>Convert to Lowercase</button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setText("")}>Clear Text</button>
+
+      </div>
+    </>
   )
 }
 
